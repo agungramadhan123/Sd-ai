ede5483a1e33ef7acd8dbb72c6e41e76b83741eb
agung <m.agungramadhan08@gmail.com>
2026-05-04 23:57:54 +0700

    update + tambah fitur

--- Ml/processing.py
+++ Ml/processing.py
@@ -25,21 +25,46 @@ class Preprocessing(ProsesData):
         text = re.sub(r'@\w+', '', str(text))
         text = re.sub(r'#\w+', '', str(text))
         return text
+    
     def hapus_tanda_baca(self,teks:str):
         return teks.translate(str.maketrans('', '', string.punctuation))
-    def hapus_angka(self,teks:str):
-        return re.sub(r'\d+', '', teks)
+
 
     def hapus_emoji(self,teks:str):
         return re.sub(r'[^\x00-\x7F]+', '', teks)
     
+    def hapus_placeholder(self, text: str):
+        text = re.sub(r'\{@[^}]*@\}', '', str(text))
+        text = re.sub(r'\{\{[^}]*\}\}', '', text)
+        return text
+
     def jalankan_cleaning(self, text: str):
-        """Fungsi praktis untuk menjalankan semua proses cleaning sekaligus"""
+        text = str(text).lower()
         text = self.hapus_url(text)
         text = self.hapus_tag(text)
         text = self.hapus_mention_dan_hashtag(text)
+        text = self.hapus_placeholder(text)
         text = re.sub(r'\s+', ' ', text).strip()
         text = self.hapus_tanda_baca(text)
-        text = self.hapus_angka(text)
         text = self.hapus_emoji(text)
+        return text
+
+    def tokenize_dan_hapus_stopwords(self, text: str):
+        from nltk.corpus import stopwords
+        from nltk.tokenize import word_tokenize
+
+        stop_words = set(stopwords.words('english'))
+        custom_stopwords = {
+            'username', 'url', 'via',
+            'rt', 'amp',
+        }
+        stop_words.update(custom_stopwords)
+
+        tokens = word_tokenize(text)
+        tokens = [t for t in tokens if t not in stop_words and len(t) > 1]
+        return ' '.join(tokens)
+    
+    def preprocess_full(self, text: str):
+        text = self.jalankan_cleaning(text)
+        text = self.tokenize_dan_hapus_stopwords(text)
         return text
